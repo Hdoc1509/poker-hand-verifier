7d30fe6a93f7ac83cca5e21385b90c21422a9b58
hdoc <blueguitarist1520@gmail.com>
2022-07-05 18:34:08 -0500

    Feat: Added util is-valid-card.ts

---
+++ src/utils/is-valid-card.ts
@@ -0,0 +1,20 @@
+const VALID_NUMBER = /^A|[2-9]|10|J|Q|K$/;
+const VALID_SUIT = /^H|S|C|D$/;
+
+export const NOT_FOUND_INVALID_INDEX = -1;
+
+export const isValidCard = (card: any): boolean => {
+  if (card?.number == null || card?.suit == null) return false;
+
+  const { number, suit } = card;
+
+  if (typeof number !== 'string' || typeof suit !== 'string') return false;
+
+  if (number.match(VALID_NUMBER) === null || suit.match(VALID_SUIT) === null)
+    return false;
+
+  return true;
+};
+
+export const getInvalidCardIndex = (cards: Array<any>): number =>
+  cards.findIndex((card) => !isValidCard(card));
