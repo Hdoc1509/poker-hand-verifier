4f9d47b1ae509f074f66e421bc0b302c1ab4d579
hdoc <blueguitarist1520@gmail.com>
2022-07-05 18:35:05 -0500

    Test: Added and passed tests for is-valid-card.ts

---
+++ src/utils/is-valid-card.test.ts
@@ -0,0 +1,45 @@
+// @ts-ignore
+import { toBeFalse, toBeTrue } from 'jest-extended';
+import {
+  getInvalidCardIndex,
+  isValidCard,
+  NOT_FOUND_INVALID_INDEX,
+} from './is-valid-card';
+import {
+  invalidCard,
+  invalidCards,
+  validCard,
+} from '../../global-test-variables';
+
+expect.extend({ toBeFalse, toBeTrue });
+
+describe('--- isValidCard() ---', () => {
+  test('Invalid card returns false', () => {
+    expect(isValidCard('not a card')).toBeFalse();
+    expect(isValidCard([])).toBeFalse();
+    expect(isValidCard({})).toBeFalse();
+    expect(isValidCard(invalidCard)).toBeFalse();
+  });
+
+  test('Valid card returns true', () => {
+    expect(isValidCard({ number: '10', suit: 'H' })).toBeTrue();
+    expect(isValidCard({ suit: 'C', number: 'A' })).toBeTrue();
+    expect(isValidCard(validCard)).toBeTrue();
+  });
+});
+
+describe('--- getInvalidCardIndex() ---', () => {
+  test('If there is no invalid card returns -1', () => {
+    expect(
+      getInvalidCardIndex([
+        { number: '3', suit: 'H' },
+        { number: 'K', suit: 'D' },
+      ])
+    ).toBe(NOT_FOUND_INVALID_INDEX);
+  });
+
+  test('Getting the index of invalid card correctly', () => {
+    expect(getInvalidCardIndex(invalidCards)).toBe(0);
+    expect(getInvalidCardIndex([{ number: 'A', suit: 'C' }, 'card'])).toBe(1);
+  });
+});
