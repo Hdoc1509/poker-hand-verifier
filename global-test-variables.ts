680813d5b1142dfb09538b5afafb7c0d14e3742a
hdoc <blueguitarist1520@gmail.com>
2022-07-05 18:24:52 -0500

    Test: Added file with global variables for tests

---
+++ global-test-variables.ts
@@ -0,0 +1,7 @@
+export const invalidCards = [{ invalidProp: 'anything', suit: 'H' }, {}];
+
+export const oneCard = [{ number: '1', suit: 'C' }];
+
+export const invalidCard = { invalid: true, prop: 'not allowed' };
+
+export const validCard = { number: 'A', suit: 'S' };
