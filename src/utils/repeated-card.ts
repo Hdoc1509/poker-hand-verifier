f66effecb84a2ab67db83ac155a0db2716ad0a35
hdoc <blueguitarist1520@gmail.com>
2022-07-07 19:31:56 -0500

    Test: Added missing logic an validations to getRepeatedCard() for pass tests

--- src/utils/repeated-card.ts
+++ src/utils/repeated-card.ts
@@ -1,11 +1,14 @@
 import { Card } from '../index';
 
 export const getRepeatedCard = (cards: Array<Card>): string => {
-  const repeatedCards = new Set(
-    cards.map(({ number, suit }) => `${number}${suit}`)
-  );
+  const stringCards = cards.map(({ number, suit }) => `${number}${suit}`);
+  const uniqueCards = Array.from(new Set(stringCards));
 
-  return repeatedCards.size !== cards.length
-    ? Array.from(repeatedCards)[0]
-    : undefined;
+  const repeatedCards = uniqueCards.filter((card) => {
+    const regExp = new RegExp(card, 'g');
+
+    return stringCards.join('').match(regExp)?.length >= 2;
+  });
+
+  if (repeatedCards.length > 0) return repeatedCards[0];
 };
