cadf5ef2f02a9bd153cd605e98a714b6acff698b
hdoc <blueguitarist1520@gmail.com>
2022-07-09 19:30:53 -0500

    Test(isPair.test.ts): Changed expected error messages correctly

--- tests/isPair.test.ts
+++ tests/isPair.test.ts
@@ -15,19 +15,25 @@ const invalidCard = { myNum: 'three' };
 expect.extend({ toBeFalse, toBeTrue });
 
 describe('--- isPair() ---', () => {
-  test('No argument throws an Error', () => {
-    expect(() => isPair()).toThrow('Expected 2 arguments, but received 0');
-  });
+  const minimum = 5;
 
-  test('Only one argument throws an Error', () => {
-    expect(() => isPair(validCards)).toThrow(
-      'Expected 2 arguments, but received 1. Missing "number" argument for check its pair'
+  test('No argument throws an Error', () => {
+    expect(() => isPair()).toThrow(
+      `Invalid Array of cards. ${ERROR_MESSAGE.QuantityCards(0, { minimum })}`
     );
   });
 
-  test('First argument is not a valid Array of cards throws an specific Error', () => {
-    const minimum = 5;
+  test('Only one argument throws an Error', () => {
+    expect(() =>
+      isPair([
+        ...validCards,
+        { number: '8', suit: 'D' },
+        { number: '9', suit: 'H' },
+      ])
+    ).toThrow('Argument "number" must be an String');
+  });
 
+  test('First argument is not a valid Array of cards throws an specific Error', () => {
     expect(() => isPair(false as unknown as Array<Card>, '5')).toThrow(
       `Invalid Array of cards. ${ERROR_MESSAGE.NotArray}`
     );
@@ -51,7 +57,7 @@ describe('--- isPair() ---', () => {
 
     expect(() =>
       isPair([validCards[0], ...validCards, { number: 'J', suit: 'D' }], 'K')
-    ).toThrow(`Invalid Array of cards. ${ERROR_MESSAGE.RepeatedCard('KH')}`);
+    ).toThrow(`Invalid Array of cards. ${ERROR_MESSAGE.RepeatedCard('5C')}`);
   });
 
   test('Hand is not a PAIR of a specific number returns false', () => {
