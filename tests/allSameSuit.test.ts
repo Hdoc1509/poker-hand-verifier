54754c76b80259ade4c168ef136f23328dc51206
hdoc <blueguitarist1520@gmail.com>
2022-07-11 14:44:22 -0500

    Test(allSameSuit.test.ts): Removed unnecesary tests and updated one error test

--- tests/allSameSuit.test.ts
+++ tests/allSameSuit.test.ts
@@ -1,14 +1,10 @@
 // @ts-ignore
 import { toBeFalse, toBeTrue } from 'jest-extended';
-import { Card } from '../src';
 import { allSameSuit } from '../src/other-checkings';
-import { ERROR_MESSAGE } from '../src/utils/validate-cards';
 
 expect.extend({ toBeFalse, toBeTrue });
 
-const invalidCards = [{ number: '6', suit: 'H' }, 'invalid'];
 const oneCard = [{ number: 'K', suit: 'D' }];
-const repeatedCard = [...oneCard, ...oneCard];
 const differentSuit = [
   { number: '9', suit: 'C' },
   { number: '10', suit: 'S' },
@@ -19,34 +15,8 @@ const sameSuit = [
 ];
 
 describe('--- allSameSuit() ---', () => {
-  test('No argument throws an Error', () => {
-    expect(() => allSameSuit()).toThrow(ERROR_MESSAGE.QuantityCards(0));
-  });
-
-  test('Argument is not an Array throws an Error', () => {
-    expect(() => allSameSuit({} as Array<Card>)).toThrow(
-      ERROR_MESSAGE.NotArray
-    );
-  });
-
-  test('Empty Array as argument throws an Error', () => {
-    expect(() => allSameSuit([])).toThrow(ERROR_MESSAGE.QuantityCards(0));
-  });
-
-  test('Array with invalid cards throws an Error', () => {
-    expect(() => allSameSuit(invalidCards as Array<Card>)).toThrow(
-      ERROR_MESSAGE.InvalidCard(1)
-    );
-  });
-
   test('Array that have less than 2 cards throws an Error', () => {
-    expect(() => allSameSuit(oneCard)).toThrow(ERROR_MESSAGE.QuantityCards(1));
-  });
-
-  test('Array with repeated cards throws an Error', () => {
-    expect(() => allSameSuit(repeatedCard)).toThrow(
-      ERROR_MESSAGE.RepeatedCard('KD')
-    );
+    expect(() => allSameSuit(oneCard)).toThrow('Expected an Array of minimum 2 cards');
   });
 
   test('All cards have not the same suit returns false', () => {
