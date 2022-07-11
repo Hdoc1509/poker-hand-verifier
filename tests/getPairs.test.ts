e4f9a09950d72ffd252035e7b3df40a10f9a1ea0
hdoc <blueguitarist1520@gmail.com>
2022-07-11 14:24:57 -0500

    Test(getPairs.test.ts): Used varibles for getPairs() argument

--- tests/getPairs.test.ts
+++ tests/getPairs.test.ts
@@ -8,30 +8,30 @@ const validCards = [
 
 describe('--- getPairs() ---', () => {
   test("If there are no PAIR's returns an empty Set", () => {
-    expect(
-      getPairs([
-        ...validCards,
-        { number: 'A', suit: 'H' },
-        { number: 'J', suit: 'D' },
-      ])
-    ).toEqual(new Set());
+    const cards = [
+      ...validCards,
+      { number: 'A', suit: 'H' },
+      { number: 'J', suit: 'D' },
+    ];
+
+    expect(getPairs(cards)).toEqual(new Set());
   });
 
   test("If there are PAIR's returns a Set with its numbers correctly", () => {
-    expect(
-      getPairs([
-        ...validCards,
-        { number: 'Q', suit: 'C' },
-        { number: '7', suit: 'D' },
-      ])
-    ).toEqual(new Set(['Q']));
+    const cards1 = [
+      ...validCards,
+      { number: 'Q', suit: 'C' },
+      { number: '7', suit: 'D' },
+    ];
 
-    expect(
-      getPairs([
-        ...validCards,
-        { number: '2', suit: 'C' },
-        { number: '6', suit: 'S' },
-      ])
-    ).toEqual(new Set(['2', '6']));
+    const cards2 = [
+      ...validCards,
+      { number: '2', suit: 'C' },
+      { number: '6', suit: 'S' },
+    ];
+
+    expect(getPairs(cards1)).toEqual(new Set(['Q']));
+
+    expect(getPairs(cards2)).toEqual(new Set(['2', '6']));
   });
 });
