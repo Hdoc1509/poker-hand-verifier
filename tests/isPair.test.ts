ff5f579aada817cc1297e8269fe837e10b8e4168
hdoc <blueguitarist1520@gmail.com>
2022-07-12 18:43:32 -0500

    Test(isPair.test.ts): Move variables out of tests closure and rename 'validCards' to 'cards'

--- tests/isPair.test.ts
+++ tests/isPair.test.ts
@@ -2,32 +2,28 @@
 import { toBeFalse, toBeTrue } from 'jest-extended';
 import { isPair } from '../src/pair';
 
-const validCards = [
+const cards = [
   { number: '5', suit: 'C' },
   { number: '7', suit: 'D' },
   { number: 'K', suit: 'H' },
 ];
 
+const noPair = [
+  ...cards,
+  { number: 'K', suit: 'D' },
+  { number: '8', suit: 'S' },
+];
+
+const pair = [...cards, { number: '5', suit: 'D' }, { number: 'Q', suit: 'D' }];
+
 expect.extend({ toBeFalse, toBeTrue });
 
 describe('--- isPair() ---', () => {
   test('Hand is not a PAIR of a specific number returns false', () => {
-    const cards = [
-      ...validCards,
-      { number: 'K', suit: 'D' },
-      { number: '8', suit: 'S' },
-    ];
-
-    expect(isPair(cards, 'Q')).toBeFalse();
+    expect(isPair(noPair, 'Q')).toBeFalse();
   });
 
   test('Hand is PAIR of a specific number returns true', () => {
-    const cards = [
-      ...validCards,
-      { number: '5', suit: 'D' },
-      { number: 'Q', suit: 'D' },
-    ];
-
-    expect(isPair(cards, '5')).toBeTrue();
+    expect(isPair(pair, '5')).toBeTrue();
   });
 });
