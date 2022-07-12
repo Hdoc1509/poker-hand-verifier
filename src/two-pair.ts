6acfb5b2601a1499bb2101db6f181d777674678a
hdoc <blueguitarist1520@gmail.com>
2022-07-12 17:14:14 -0500

    Refactor(two-pair.ts): Removed unnecesary function isTwoPair()

--- src/two-pair.ts
+++ src/two-pair.ts
@@ -11,18 +11,3 @@ export const isAnyTwoPair = (cards: Array<Card>): boolean => {
 /** Searchs for any possible TWO PAIR and returns its card numbers */
 export const findTwoPair = (cards: Array<Card>): Array<string> =>
   Array.from(getPairs(cards));
-
-/** Check if hand is an specific TWO PAIR */
-export const isTwoPair = (
-  cards: Array<Card>,
-  [{ number: number1 }, { number: number2 }]: Array<Card>
-): boolean => {
-  const number1Matches = cards.filter(({ number }) => number === number1);
-  const number2Matches = cards.filter(({ number }) => number === number2);
-
-  return (
-    number1Matches.length === 2 &&
-    number2Matches.length === 2 &&
-    number1 !== number2
-  );
-};
