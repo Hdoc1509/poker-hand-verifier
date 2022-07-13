ae90bf8ad45f2076c493cba7f3b5ae91747e7b42
hdoc <blueguitarist1520@gmail.com>
2022-07-13 10:37:04 -0500

    Docs(two-pair.ts): Improve description of function findTwoPair()

--- src/two-pair.ts
+++ src/two-pair.ts
@@ -1,7 +1,7 @@
 import { getPairs } from './pair';
 import { Card } from './index';
 
-/** Searchs for any possible TWO PAIR and returns its card numbers */
+/** Searchs for any possible TWO PAIR and returns its card numbers, otherwise returns undefined */
 export const findTwoPair = (cards: Array<Card>): Array<string> => {
   const pairs = getPairs(cards);
 
