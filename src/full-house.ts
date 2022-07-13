1d3b3774739f5addb5c7a0d766a83630124e4db4
hdoc <blueguitarist1520@gmail.com>
2022-07-13 18:44:27 -0500

    Refactor(full-house.ts): Remove unnecesary function isAnyFullHouse()

--- src/full-house.ts
+++ src/full-house.ts
@@ -7,14 +7,6 @@ export type FullHouseData = {
   threeOfKind: string;
 };
 
-/** Check if hand is any possible FULL HOUSE */
-export const isAnyFullHouse = (cards: Array<Card>): boolean => {
-  const pairs = getPairs(cards);
-  const threeOfKind = getThreeOfKind(cards);
-
-  return pairs.size === 1 && threeOfKind.size === 1;
-};
-
 /** If hand is a FULL HOUSE retrieves its composing numbers, otherwise returns undefined */
 export const findFullHouse = (cards: Array<Card>): FullHouseData => {
   const [pair] = Array.from(getPairs(cards));
