ba7f00c37499ec25b307108d07b854f7de6c1d3b
hdoc <blueguitarist1520@gmail.com>
2022-07-13 18:49:05 -0500

    Refactor(four-of-kind.ts): Change type of parameter 'number' in isFourOfKind()

--- src/four-of-kind.ts
+++ src/four-of-kind.ts
@@ -2,7 +2,7 @@ import { numberMatches } from './other-checkings';
 import { Card } from './index';
 
 /** Check if hand is an specific FOUR OF A KIND */
-export const isFourOfKind = (cards: Array<Card>, { number }: Card): boolean => {
+export const isFourOfKind = (cards: Array<Card>, number: string): boolean => {
   const { matches } = numberMatches(cards, number);
 
   return matches.length === 4;
@@ -10,8 +10,8 @@ export const isFourOfKind = (cards: Array<Card>, { number }: Card): boolean => {
 
 /** Check if hand is any possible FOUR OF A KIND */
 export const isAnyFourOfKind = (cards: Array<Card>) =>
-  cards.some((card) => isFourOfKind(cards, card));
+  cards.some(({ number }) => isFourOfKind(cards, number));
 
 /** Searchs for any possible FOUR OF A KIND and returns its card number */
 export const findFourOfKind = (cards: Array<Card>): string =>
-  cards.find((card) => isFourOfKind(cards, card)).number;
+  cards.find(({ number }) => isFourOfKind(cards, number)).number;
