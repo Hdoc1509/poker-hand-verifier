3c113523d69cd251c2f2c06c5a82fafe1c38a8b0
hdoc <blueguitarist1520@gmail.com>
2022-07-13 11:38:03 -0500

    Test: Added and passed test for isStraight()

---
+++ tests/isStraight.test.ts
@@ -0,0 +1,55 @@
+// @ts-ignore
+import { toBeFalse, toBeTrue } from 'jest-extended';
+import { isStraight } from '../src/straight';
+
+const cards = [
+  { number: '5', suit: 'D' },
+  { number: '6', suit: 'D' },
+  { number: '7', suit: 'D' },
+];
+
+const noStraight = [
+  ...cards,
+  { number: '5', suit: 'H' },
+  { number: 'A', suit: 'C' },
+];
+
+const straight = [
+  ...cards,
+  { number: '4', suit: 'C' },
+  { number: '8', suit: 'H' },
+];
+
+const flushStraight = [
+  ...cards,
+  { number: '8', suit: 'D' },
+  { number: '9', suit: 'D' },
+];
+
+const royalFlush = [
+  { number: '10', suit: 'S' },
+  { number: 'J', suit: 'S' },
+  { number: 'Q', suit: 'S' },
+  { number: 'K', suit: 'S' },
+  { number: 'A', suit: 'S' },
+];
+
+expect.extend({ toBeFalse, toBeTrue });
+
+describe('--- isStraight() ---', () => {
+  test('Hand is not an specific STRAIGHT returns false', () => {
+    expect(isStraight(noStraight, '5-9')).toBeFalse();
+  });
+
+  test('Hand is a specific STRAIGHT returns true', () => {
+    expect(isStraight(straight, '4-8')).toBeTrue();
+  });
+
+  test('Hand is also a FLUSH STRAIGHT returns true', () => {
+    expect(isStraight(flushStraight, '5-9')).toBeTrue();
+  });
+
+  test('Hand is also a ROYAL FLUSH returns true', () => {
+    expect(isStraight(royalFlush, '10-A')).toBeTrue();
+  });
+});
