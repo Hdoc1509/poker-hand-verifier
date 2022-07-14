074ffa7ea71fa1cd0270fb6e5ba1462c4110ddea
hdoc <blueguitarist1520@gmail.com>
2022-07-14 18:08:17 -0500

    Test(high-card.ts): Add missing validation for pass test of verificateHand()

--- src/high-card.ts
+++ src/high-card.ts
@@ -1,9 +1,13 @@
-import { numberMatches } from './other-checkings';
+import {
+  allDifferentNumbers,
+  allSameSuit,
+  numberMatches,
+} from './other-checkings';
 import { Card } from './index';
 
 /** Check if hand is HIGH CARD */
 export const isHighCard = (cards: Array<Card>): boolean => {
   const { matches: aces } = numberMatches(cards, 'A');
 
-  return aces.length === 1;
+  return aces.length === 1 && allDifferentNumbers(cards) && !allSameSuit(cards);
 };
