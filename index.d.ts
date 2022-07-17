0f5bc2291f1ed38b869cf73802ae165af6c67a69
hdoc <blueguitarist1520@gmail.com>
2022-07-17 13:15:15 -0500

    Package: Update type declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,12 @@
-declare module 'index' {
-  export type Card = {
-    number: string;
-    suit: string;
-  };
-  export type HandData = {
-    cards: Array<string>;
-    description: string;
-    type: string;
-  };
-  /** Verificate an specific hand and returns its data */
-  export const verificateHand: (cards: Array<Card>) => HandData;
-}
+export type Card = {
+  number: string;
+  suit: string;
+};
+export type HandData = {
+  cards: Array<string>;
+  description: string;
+  type: string;
+};
+
+/** Verificate an specific hand and returns its data */
+export declare function verificateHand(cards: Array<Card>): HandData;
